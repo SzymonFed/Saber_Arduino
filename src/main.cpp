e36618601a33a2a9d0ec3a09e5e4b1f75c97a2c2
SzymonFed <szymonfederkiewicz@gmail.com>
2021-10-17 21:47:51 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <SparkFunLSM9DS1.h>
+#include <DFRobotDFPlayerMini.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
